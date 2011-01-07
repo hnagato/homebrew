d56cdfdbb2df22f40fe23035d0c7fd5567f8d791
z2++ <h.nagato@gmail.com>
2011-01-07 07:43:37 -0800

    Update formula for screen

--- Library/Formula/screen.rb
+++ Library/Formula/screen.rb
@@ -4,18 +4,16 @@ class Screen <Formula
   head 'git://git.savannah.gnu.org/screen.git', :using => :git
   homepage 'http://www.gnu.org/software/screen/'
 
-  # depends_on 'cmake'
-
   def install
-    Dir.chdir "./src"
-    system "/bin/sh autogen.sh"
-    system "./configure", "--enable-colors256", "--enable-locale",
-                          "--enable-pam", "--enable-telnet",
-                          "--with-sys-screenrc=/etc/screenrc",
-                          "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
-    # system "cmake . #{std_cmake_parameters}"
-    system "make"
-    system "make install"
+    cd 'src' do
+      system "/bin/sh autogen.sh"
+      system "./configure", "--enable-colors256", "--enable-locale",
+                            "--enable-pam", "--enable-telnet",
+                            "--with-sys-screenrc=/etc/screenrc",
+                            "--disable-debug", "--disable-dependency-tracking",
+                            "--prefix=#{prefix}"
+      system "make"
+      system "make install"
+    end
   end
 end
